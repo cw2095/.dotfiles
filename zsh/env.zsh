b018bb5ebd49e11958c25b3d191691152fe96c09
cw <879173147@qq.com>
2021-01-20 10:20:54 +0800

    add zsh config

---
+++ zsh/env.zsh
@@ -0,0 +1,9 @@
+export XDG_CONFIG_HOME=$HOME/.config
+export LOCALBIN=$HOME/.dotfiles/bin
+export PATH=$PATH:$LOCALBIN
+
+export TERM=xterm-256color
+export TERM_ITALICS=true
+
+export ZSH_AUTOSUGGEST_USE_ASYNC=1
+export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
